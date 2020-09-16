a858c92906564f3c5c997aea2b1bcdfd76653a55
blazingdragon2020 <59397848+blazingdragon2020@users.noreply.github.com>
2020-09-16 11:26:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -543,7 +543,22 @@ client.on ("message"  , async message  =>  {
 
 
     }
+if(message.content.startsWith(prefix + 'purge')){
 
+        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('Koi zaroorat nahi hai beta!')
+        if(!args[1]) return message.channel.send("Specify how many messages u wanna delete u....")
+        delength = args[1]
+        if(!delength || delength == 0){
+            message.channel.send("I can't delete 0 messages,u mofo")
+            return 
+         } 
+
+    await message.channel.bulkDelete(delength)
+  .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
+  .catch(console.error);
+  message.channel.send(`Deleted ${delength} messages!!`)
+    }
+    
 
 
 
